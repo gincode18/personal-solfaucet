7db929c8433f42e1a6f861dc35049f16b72c1d05
gincode18 <vishalkamboj9211@gmail.com>
2024-12-29 16:59:10 +0530

    airdrop working :)

--- src/components/AirDrop.tsx
+++ src/components/AirDrop.tsx
@@ -3,12 +3,13 @@ import { useConnection } from "@solana/wallet-adapter-react";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { useToast } from "@/hooks/use-toast";
 import ShinyButton from "./ui/shiny-button";
+import confetti from "canvas-confetti";
 
-type props={
-    amount:number
-  }
+type props = {
+  amount: number
+}
 
-export function RequestAirdrop({amount}:props) {
+export function RequestAirdrop({ amount }: props) {
   const { toast } = useToast()
   const wallet = useWallet();
   const { connection } = useConnection();
@@ -29,7 +30,33 @@ export function RequestAirdrop({amount}:props) {
         title: "Airdrop SucessFull",
         description: `Airdropped ${amount} SOL to ${wallet.publicKey.toBase58()}`,
       });
+      const end = Date.now() + 3 * 1000; // 3 seconds
+      const colors = ["#a786ff", "#fd8bbc", "#eca184", "#f8deb1"];
 
+      const frame = () => {
+        if (Date.now() > end) return;
+
+        confetti({
+          particleCount: 2,
+          angle: 60,
+          spread: 55,
+          startVelocity: 60,
+          origin: { x: 0, y: 0.5 },
+          colors: colors,
+        });
+        confetti({
+          particleCount: 2,
+          angle: 120,
+          spread: 55,
+          startVelocity: 60,
+          origin: { x: 1, y: 0.5 },
+          colors: colors,
+        });
+
+        requestAnimationFrame(frame);
+      };
+
+      frame();
     } catch (error) {
       console.error("Failed to request airdrop:", error);
       toast({
@@ -42,7 +69,7 @@ export function RequestAirdrop({amount}:props) {
 
   return (
     <div>
-     <ShinyButton onClick={requestAirdrop}>DevNet</ShinyButton> 
+      <ShinyButton onClick={requestAirdrop}>Airdrop</ShinyButton>
     </div>
   );
 }
